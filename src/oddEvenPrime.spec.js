6789fbd1322168044e585a78640d9545cd7ab92c
Gissela0611 <76925481+Gissela0611@users.noreply.github.com>
2022-12-31 12:36:49 -0500

    Add files via upload

---
+++ src/oddEvenPrime.spec.js
@@ -0,0 +1,41 @@
+
+import { getOddEvenPrime } from "./oddEvenPrime";
+
+describe('Odd Even Prime code kata', () => {
+  test('should return "Odd" if the number is 1', () => {
+    const value = 1;
+    const expectedResult = 'Odd';
+    const resp = getOddEvenPrime(value);
+    expect(resp).toEqual(expectedResult);
+  });
+  test('should return "Odd" if the number is 9', () => {
+    const value = 9;
+    const expectedResult = 'Odd';
+    const resp = getOddEvenPrime(value);
+    expect(resp).toEqual(expectedResult);
+  });
+  test('should return "Even" if the number is 4', () => {
+    const value = 4;
+    const expectedResult = 'Even';
+    const resp = getOddEvenPrime(value);
+    expect(resp).toEqual(expectedResult);
+  });
+  test('should return "Even" if the number is 14', () => {
+    const value = 14;
+    const expectedResult = 'Even';
+    const resp = getOddEvenPrime(value);
+    expect(resp).toEqual(expectedResult);
+  });
+  test('should return "2" if the number is 2', () => {
+    const value = 2;
+    const expectedResult = '2';
+    const resp = getOddEvenPrime(value);
+    expect(resp).toEqual(expectedResult);
+  });
+  test('should return "7" if the number is 7', () => {
+    const value = 7;
+    const expectedResult = '7';
+    const resp = getOddEvenPrime(value);
+    expect(resp).toEqual(expectedResult);
+  });
+});
